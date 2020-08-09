1d1d0d4c7ed8ec30f0d48b05f034b50b071dfcaf
Flamefusion <meshekharbehera613@gmail.com>
2020-08-10 00:48:20 +0530

    X00P: Nuke power HAL Signed-off-by: Flamefusion <meshekharbehera613@gmail.com>

--- device.mk
+++ device.mk
@@ -306,12 +306,6 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QMI
 PRODUCT_PACKAGES += \
