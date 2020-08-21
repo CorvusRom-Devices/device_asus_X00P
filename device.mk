011eeb445aa6466c7d7af2e2b9f32cd9ce9c8081
Flamefusion <meshekharbehera613@gmail.com>
2020-08-22 01:39:58 +0530

    X00P: Make separate init script for zenparts and move all the zenparts related permission to init.zen_parts.rc Signed-off-by: Flamefusion <meshekharbehera613@gmail.com>

--- device.mk
+++ device.mk
@@ -348,7 +348,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.zen_parts.rc 
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
